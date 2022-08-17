fbd20c6542f4f318b10f61a46562eb820f44341e
Martin Villarreal <martinvictorvillarreal@gmail.com>
2022-08-17 16:46:13 -0300

    dbConfig update

--- src/dbConfig.js
+++ src/dbConfig.js
@@ -1,8 +1,10 @@
 import dotenv from "dotenv";
 dotenv.config();
 
+const mongoDbPassword = process.env.MONGODBPASSWORD
+
 export default {
     mongodb: {
-      connectionString: "mongodb+srv://VillarrealDev:" + process.env.MONGODBPASSWORD + "@cluster0.oc4hq9p.mongodb.net/?retryWrites=true&w=majority",
+      connectionString: "mongodb+srv://VillarrealDev:" + mongoDbPassword + "@cluster0.oc4hq9p.mongodb.net/?retryWrites=true&w=majority",
     }
   };
