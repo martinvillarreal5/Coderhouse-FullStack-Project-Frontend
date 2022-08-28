b7399856b18c7d47fab1e333f91325f5fbd96548
Martin Villarreal <martinvictorvillarreal@gmail.com>
2022-08-28 18:11:41 -0300

    Updated mongo container

--- src/containers/MongoContainer.js
+++ src/containers/MongoContainer.js
@@ -2,10 +2,11 @@
 import mongoose from "mongoose";
 import dbConfig from "../dbConfig.js";
 
-await mongoose.connect(dbConfig.mongodb.connectionString,)
+await mongoose.connect(dbConfig.mongodb.connectionString,) 
+// move to another file using crete conection, the usea a conection instead of mongoose object
     .then(() => console.log("DB Connection Successfull!"))
-    .catch((err) => {
-        console.log('Error en la conección con mongodb: ', err);
+    .catch((error) => {
+        console.log('Error en la conección con mongodb: ', error);
     })
 //research when to close a conection
 
@@ -15,73 +16,57 @@ class MongoContainer {
         //console.log(this.model.collection.collectionName)
     }
 
-    async getById(id) {
-        try {
-            const object = await this.model.findById(id);
-            return object ? object : null
-        } catch (err) {
-            console.log('Error in getById Method: ', err);
-        }
+    getById(id) {
+        return this.model.findById(id);
     }
+/* 
+    checkExistance(id){
+        
+    } */
 
     async getAll() {
         try {
             const array = await this.model.find({})
             // find returns a empty array if didnt found a doc
             return array ? array : null;
-        } catch (err) {
-            console.log('Error in getAll Method: ', err);
+        } catch (error) {
+            console.log('Error in getAll Method: ', error);
         }
     }
 
     async save(object) {
-        try {
-            console.log(object)
-            object.timestamp = Date.now(); //aqui?
-            const newObject = new this.model(object);
-            const savedObject = await newObject.save();
-            return savedObject._id;
-        } catch (err) {
-            console.log('Error in Save Method: ', err);
-        }
+        object.timestamp = Date.now(); //aqui?
+        const newObject = new this.model(object);
+        const savedObject = await newObject.save();
+        return savedObject._id;
     }
 
     async updateById(id, data) {
-        //cambiar metodo en cartDao, o hacer que no tenga efecto en el mismo
-        try {
-            const updatedObject = await this.model.findByIdAndUpdate(
-                id,
-                //{runValidators: true}, parece que tiene limitaciones essta validacion, luego cambiar a un find y luego un save con validacion entre medio
-                {
-                    $set: data, //validar data para que no sobreEscriba campos que no deben cambiarse? (como _id, timestamp, etc )
-                },
-                { new: true } // sin esto, la funcion devuelve el objeto anterior a la modificacion
-            );
-            return (updatedObject._id);
-        } catch (err) {
-            console.log('Error in Update Method: ', err);
-        }
+        //cambiar a metodos especificos para cada dao, o hacer esto validando en cada service mejor?
+        const updatedObject = await this.model.findByIdAndUpdate(
+            id,
+            //{runValidators: true}, parece que tiene limitaciones essta validacion, luego cambiar a un find y luego un save con validacion entre medio
+            {
+                $set: data, //validar data para que no sobreEscriba campos que no deben cambiarse? (como _id, timestamp, etc )
+            },
+            { new: true } // sin esto, la funcion devuelve el objeto anterior a la modificacion
+        );
+        return (updatedObject._id); //cambiar para que devuelva el nuevo objeto
     }
     async deleteById(id) {
-        // Elimina del archivo el objeto con el id buscado.
-        try {
-            const deletedProduct = await this.model.findByIdAndRemove(
-                id 
-                // buscar parametro para que solo devuelva la id, y cambiarlos
-            );
-            return deletedProduct;
-        } catch (err) {
-            console.log('Error in deleteById Method: ', err);
-        }
+        const deletedProduct = await this.model.findByIdAndRemove(
+            id
+            // buscar parametro para que solo devuelva la id, y cambiarlos
+        );
+        return deletedProduct;
     }
 
-    async deleteAll() {
-        // Elimina todos los objetos presentes en el archivo.
-        const collectionName = this.model.collection.collectionName //async??
+    async deleteAll() { //Empty the collection
+        const collectionName = this.model.collection.collectionName
         try {
-            await mongoose.connection.db.dropCollection(collectionName)//async??
-        } catch (err) {
-            console.log('Error in deleteAll Method: ', err);
+            await mongoose.connection.db.dropCollection(collectionName)
+        } catch (error) {
+            console.log('Error in deleteAll Method: ', error);
         }
     }
 }
