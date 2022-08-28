c70125213e303585ea11b09c82dfd9d79f70ce22
Martin Villarreal <martinvictorvillarreal@gmail.com>
2022-08-28 18:12:13 -0300

    updated services

--- src/services/cart/cartServices.js
+++ src/services/cart/cartServices.js
@@ -1,56 +1,38 @@
 import { CartDao } from '../../daos/index.js';
 
 const getCartById = async (id) => {
-    try {
         const cart = await CartDao.getById(id);
-        if (!cart) throw 'Cart not found'; //necesary?
+        //if (!cart) throw 'Cart not found'; //va en esta capa
         return cart;
-    } catch (error) {
-        console.log('Error: ', error);
-    }
 }
 
 const getCarts = async () => {
-    try {
         return await CartDao.getAll();
-    } catch (err) {
         console.log('Error: ', error);
-    }
 }
 const saveCart = async (data) => {
-    try {
         console.log(data)
         const cart = data;
         const savedCartId = CartDao.save(cart);
         return savedCartId; 
-    } catch (error) {
-        console.log('Error: ', error);
-    }
 }
 
 const updateCart = async (id, data) => {
-    try {
         if (!data){
-            throw 'update Cart Data is empty or undefined'
+            throw new Error('update Cart Data is empty or undefined')
+            //esto va en el controller?
         }
-        const updatedCartId = await CartDao.updateById(id, data);
+        const updatedCartId = await CartDao.updateById(id, data);/* 
         if (id != updatedCartId){
-            throw 'updatedCartId and given Id dont match, error'
-        }
+            throw new Error('updatedCartId and given Id dont match, error')
+            //no creo que haga falta esto, mas que nada testing
+        } */
         return updatedCartId;
-        
-    } catch (error) {
-        console.log('Error: ', error);
-    }
 }
 
 const deleteCart = async (id) => {
-    try {
         const deletedCart = await CartDao.deleteById(id);
         return deletedCart;
-    } catch (error) {
-        console.log('Error: ', error);
-    }
 }
 
 export default { getCartById, getCarts, saveCart, updateCart, deleteCart } 
--- src/services/product/productServices.js
+++ src/services/product/productServices.js
@@ -3,55 +3,33 @@ import { ProductDao } from '../../daos/index.js';
 
 
 const getProductById = async (id) => {
-    try {
-        const product = await ProductDao.getById(id);
-        //if (!product) throw 'producto no encontrado';
-        return product;
-    } catch (error) {
-        console.log('Error: ', error);
-    }
+        return ProductDao.getById(id);
 }
 const getProducts = async () => {
-    try {
-        return await ProductDao.getAll();
-    } catch (err) {
-        console.log('Error: ', error);
-    }
+        const products = await ProductDao.getAll();
+        return products
 }
 const saveProduct = async (data) => {
-    try {
         const product = data;
         //validar cada dato de arriba ? o hacer eso en la squema de moongose
         const savedProductId = ProductDao.save(product);
         return savedProductId; //return saved product id
-    } catch (error) {
-        console.log('Error: ', error);
-    }
 }
 
 const updateProduct = async (id, data) => {
-    try {
         if (!data){
-            throw 'update product Data is empty or undefined'
+            throw new Error('update product Data is empty or undefined')
         }
-        const updatedProductId = await ProductDao.updateById(id, data);
+        const updatedProductId = await ProductDao.updateById(id, data);/* 
         if (id != updatedProductId){
-            throw 'updatedProductId and given Id dont match, error'
-        }
+            throw new Error('updatedProductId and given Id dont match, error')
+        } */
         return updatedProductId;
-        
-    } catch (error) {
-        console.log('Error: ', error);
-    }
 }
 
 const deleteProduct = async (id) => {
-    try {
         const deletedProduct = await ProductDao.deleteById(id);
         return deletedProduct;
-    } catch (error) {
-        console.log('Error: ', error);
-    }
 }
 
 
