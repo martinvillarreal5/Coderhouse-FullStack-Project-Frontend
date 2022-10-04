da3f009750e1ca136e673981655b45b703575e43
Martin Villarreal <martinvictorvillarreal@gmail.com>
2022-10-04 15:26:54 -0300

    auth lib

--- frontend/src/lib/authLib.js
+++ frontend/src/lib/authLib.js
@@ -1,25 +1,35 @@
-
 import axios from "axios";
 
 const logIn = async (data) => {
-    try {
-        const authResponse = await axios.post('http://localhost:8080/user/login', data)
-        console.log(authResponse.data);
-        //if (authResponse.status)
-        return authResponse
-    } catch (error) {
-        console.log(error);
-    }
+  try {
+    axios.defaults.withCredentials = true;
+    const authResponse = await axios.post(
+      "http://localhost:8080/user/login",
+      data
+    );
+    //console.log(authResponse.data);
+    //if (authResponse.status)
+    return; // authResponse;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export const logOut = async () => {
-    try {
-        axios.defaults.withCredentials = true;
-        const authResponse = await axios.post('http://localhost:8080/user/logout')
-        console.log(authResponse.data);
+  try {
+    axios.defaults.withCredentials = true;
+    const authResponse = await axios.post("http://localhost:8080/user/logout");
+    console.log(authResponse.data);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-    } catch (error) {
-        console.log(error);
-    }
-}
-
+export const SignUp = async (data) => {
+  const authResponse = await axios.post(
+    "http://localhost:8080/user/register",
+    data
+  );
+  console.log(authResponse.data);
+  return;
+};
