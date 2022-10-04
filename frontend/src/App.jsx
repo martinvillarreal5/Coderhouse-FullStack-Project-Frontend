c8225ebfb64275275bd9588b9cbb1d0a963a87af
Martin Villarreal <martinvictorvillarreal@gmail.com>
2022-10-04 15:31:07 -0300

    formated app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 import {
   MantineProvider,
@@ -10,39 +10,37 @@ import {
   Aside,
   useMantineTheme,
   Container,
-} from '@mantine/core';
-import {
-  Route,
-  Routes,
-  Link,
-} from "react-router-dom";
-import ProductListContainer from './components/product-list/productListContainer.jsx';
-import NavBar from './components/navbar/NavBar.jsx';
-import UserDashboardContainer from './components/account/profile/UserDashboardContainer.jsx';
-import LoginForm from './components/account/login/LoginForm.jsx';
-
+} from "@mantine/core";
+import { Route, Routes, Link } from "react-router-dom";
+import ProductListContainer from "./components/product-list/ProductListContainer.jsx";
+import NavBar from "./components/navbar/NavBar.jsx";
+import UserDashboardContainer from "./components/account/profile/UserDashboardContainer.jsx";
+import LoginForm from "./components/account/login/LoginForm.jsx";
+import RegisterForm from "./components/account/register/RegisterForm.jsx";
 
 function App() {
-
   const theme = useMantineTheme();
   const [opened, setOpened] = useState(false);
   return (
-
     <MantineProvider withGlobalStyles withNormalizeCSS>
       <AppShell
         styles={{
           main: {
-            background: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0],
+            background:
+              theme.colorScheme === "dark"
+                ? theme.colors.dark[8]
+                : theme.colors.gray[0],
           },
         }}
         navbarOffsetBreakpoint="sm"
         asideOffsetBreakpoint="sm"
-        navbar={<NavBar opened={opened} />
-        }
+        navbar={<NavBar opened={opened} />}
         header={
           <Header height={70} p="md">
-            <div style={{ display: 'flex', alignItems: 'center', height: '100%' }}>
-              <MediaQuery largerThan="sm" styles={{ display: 'none' }}>
+            <div
+              style={{ display: "flex", alignItems: "center", height: "100%" }}
+            >
+              <MediaQuery largerThan="sm" styles={{ display: "none" }}>
                 <Burger
                   opened={opened}
                   onClick={() => setOpened((o) => !o)}
@@ -51,31 +49,41 @@ function App() {
                   mr="xl"
                 />
               </MediaQuery>
-              <Text component={Link} to="/">Ecommerce</Text>
+              <Text component={Link} to="/">
+                Ecommerce
+              </Text>
             </div>
           </Header>
         }
         aside={
-        <MediaQuery smallerThan="sm" styles={{ display: 'none' }}>
-          <Aside p="md" hiddenBreakpoint="sm" width={{ sm: 175, lg: 250 }} sx={{ display: 'none' }}>
-            <Text>Application sidebar</Text>
-          </Aside>
-        </MediaQuery>
-      }
+          <MediaQuery smallerThan="sm" styles={{ display: "none" }}>
+            <Aside
+              p="md"
+              hiddenBreakpoint="sm"
+              width={{ sm: 175, lg: 250 }}
+              sx={{ display: "none" }}
+            >
+              <Text>Application sidebar</Text>
+            </Aside>
+          </MediaQuery>
+        }
       >
         {/* Routes here */}
         <Container size="lg">
           <Routes>
-            <Route path='/' element={<Text>Welcome</Text>} />
-            <Route path='/products' element={<ProductListContainer />} />
-            <Route path='/account/profile' element={<UserDashboardContainer/>} />
-            <Route path='/account/login' element={<LoginForm/>} />
+            <Route path="/" element={<Text>Welcome</Text>} />
+            <Route path="/products" element={<ProductListContainer />} />
+            <Route
+              path="/account/profile"
+              element={<UserDashboardContainer />}
+            />
+            <Route path="/account/login" element={<LoginForm />} />
+            <Route path="/account/signup" element={<RegisterForm />} />
           </Routes>
         </Container>
-
       </AppShell>
     </MantineProvider>
-  )
+  );
 }
 
-export default App
+export default App;
