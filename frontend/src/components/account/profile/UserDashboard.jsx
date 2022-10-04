72e0b5779d4645e5d9796df0f1def4d487b06df1
Martin Villarreal <martinvictorvillarreal@gmail.com>
2022-10-04 15:25:07 -0300

    updated userDashboard

--- frontend/src/components/account/profile/UserDashboard.jsx
+++ frontend/src/components/account/profile/UserDashboard.jsx
@@ -1,13 +1,17 @@
-import { Text } from '@mantine/core';
+import { Text } from "@mantine/core";
 
 export default function UserDashboard({ userData }) {
-    const { username, firstName, lastName, email } = userData
-    return (
-        <>
-            <Text size="xl" weight={700}>{firstName || "Placeholder" } {lastName || "Placeholder"}</Text>
-            <Text size="xs" pb="md">{username}</Text>
-            <Text size="md">📧 {email}</Text>
-
-        </>
-    )
+  const { username, firstName, lastName, email } = userData;
+  //console.log(userData);
+  return (
+    <>
+      <Text size="xl" weight={700}>
+        {firstName || "Placeholder"} {lastName || "Placeholder"}
+      </Text>
+      <Text size="xs" pb="md">
+        {username}
+      </Text>
+      <Text size="md">📧 {email}</Text>
+    </>
+  );
 }
