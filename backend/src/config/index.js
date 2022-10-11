08e78ffff99f2f87497021640973c4006001d792
Martin Villarreal <martinvictorvillarreal@gmail.com>
2022-10-11 18:14:55 -0300

    backend: updated config file

--- backend/src/config/index.js
+++ backend/src/config/index.js
@@ -1,16 +1,20 @@
-import 'dotenv/config'
+import "dotenv/config";
 
 const serverConfig = {
-    port: process.env.PORT,
-    secret: process.env.SECRET || 'mySecret'
-}
+  port: process.env.PORT,
+  secret: process.env.SECRET || "mySecret",
+};
 
 const databaseConfig = {
-    mongoDbUrl: "mongodb+srv://VillarrealDev:" + process.env.MONGODBPASSWORD + "@cluster0.oc4hq9p.mongodb.net/?retryWrites=true&w=majority",
+  mongoDbUrl:
+    "mongodb+srv://VillarrealDev:" +
+    process.env.MONGODBPASSWORD +
+    "@cluster0.oc4hq9p.mongodb.net/?retryWrites=true&w=majority",
+};
 
-}
+const mailerConfig = {
+  email: process.env.MAILER_EMAIL,
+  password: process.env.MAILER_PASS,
+};
 
-export {
-    serverConfig,
-    databaseConfig
-}
+export { serverConfig, databaseConfig, mailerConfig };
