d355a447473d1f2f9242ac18b27852f9e15bb013
Martin Villarreal <martinvictorvillarreal@gmail.com>
2022-10-10 22:03:20 -0300

    frontend: Updated user dashboard

--- frontend/src/components/account/profile/UserDashboard.jsx
+++ frontend/src/components/account/profile/UserDashboard.jsx
@@ -1,16 +1,31 @@
-import { Text } from "@mantine/core";
+import { Text, Avatar, Title } from "@mantine/core";
+import { baseServerUrl } from "../../../config/paths";
 
 export default function UserDashboard({ userData }) {
-  const { username, firstName, lastName, email } = userData;
+  const { email, firstName, lastName, avatarUrl, isAdmin } = userData;
   //console.log(userData);
   return (
     <>
-      <Text size="xl" weight={700}>
-        {firstName || "Placeholder"} {lastName || "Placeholder"}
-      </Text>
-      <Text size="xs" pb="md">
-        {username}
-      </Text>
+      <Avatar
+        src={`${baseServerUrl}/${avatarUrl}`}
+        alt="my avatar image"
+        size={150}
+        radius="xl"
+        mb="sm"
+      />
+      <Title order={2}>
+        {firstName || "Placeholder"} {lastName || "Placeholder"}{" "}
+        {isAdmin && (
+          <Text
+            inherit
+            span
+            variant="gradient"
+            gradient={{ from: "indigo", to: "cyan", deg: 45 }}
+          >
+            Admin
+          </Text>
+        )}
+      </Title>
       <Text size="md">📧 {email}</Text>
     </>
   );
