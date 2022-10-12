d2421c49a0d2c85907c013fb9e937ec316a5cf37
Martin Villarreal <martinvictorvillarreal@gmail.com>
2022-10-12 18:49:09 -0300

    backend: little update to config file

--- backend/src/config/index.js
+++ backend/src/config/index.js
@@ -1,20 +1,26 @@
 import "dotenv/config";
 
-const serverConfig = {
+export const serverConfig = {
   port: process.env.PORT,
   secret: process.env.SECRET || "mySecret",
 };
 
-const databaseConfig = {
+export const databaseConfig = {
   mongoDbUrl:
     "mongodb+srv://VillarrealDev:" +
     process.env.MONGODBPASSWORD +
     "@cluster0.oc4hq9p.mongodb.net/?retryWrites=true&w=majority",
 };
 
-const mailerConfig = {
+export const mailerConfig = {
   email: process.env.MAILER_EMAIL,
   password: process.env.MAILER_PASS,
 };
 
-export { serverConfig, databaseConfig, mailerConfig };
+export const twilioConfig = {
+  accountSid: process.env.TWILIO_SID,
+  authToken: process.env.TWILIO_AUTH_TOKEN,
+  twilioPhone: process.env.TWILIO_PHONE,
+  twilioWhatsapp: process.env.TWILIO_WHATSAPP,
+  adminPhone: "+5492616964632",
+};
