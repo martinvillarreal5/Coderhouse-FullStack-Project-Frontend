f7859e741de18f57f7b3567672ae92fefb4a888b
Martin Villarreal <martinvictorvillarreal@gmail.com>
2022-10-25 18:41:37 -0300

    Backend: fix deleProduct in prdouctController

--- backend/src/api/controllers/productController.js
+++ backend/src/api/controllers/productController.js
@@ -45,9 +45,10 @@ export const updateProduct = async (req, res, next) => {
   }
 };
 
-export const deleteProduct = async (next, req, res) => {
+export const deleteProduct = async (req, res, next) => {
   try {
-    await productServices.deleteProduct(req.params.id);
+    const id = req.params.id;
+    await productServices.deleteProduct(id);
     res.status(200).json("Product deleted");
     // ?  res.status(204).end() // también podría ser, 204 no content
   } catch (error) {
