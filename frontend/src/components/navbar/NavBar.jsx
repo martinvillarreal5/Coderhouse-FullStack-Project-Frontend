2954e0bfaecc0e46c487456ae2fa1b591d3eadf5
Martin Villarreal <martinvictorvillarreal@gmail.com>
2022-10-29 11:27:24 -0300

    Frontend: updated navbar

--- frontend/src/components/navbar/NavBar.jsx
+++ frontend/src/components/navbar/NavBar.jsx
@@ -1,54 +1,60 @@
-import { Navbar, NavLink, Button } from "@mantine/core";
-import { useEffect } from "react";
+import { Navbar, NavLink } from "@mantine/core";
 import { Link } from "react-router-dom";
 import { logOut } from "../../lib/authLib";
 import useUser from "../../hooks/useUser";
+import { useNavigate } from "react-router-dom";
 //import React, { memo } from "react";
 
 export default function NavBar(props) {
   const { opened } = props;
+  const navigate = useNavigate();
+  const { user, isLoading, mutate, isLogged, isAdmin } = useUser();
 
-  const { user, isLoading, mutate, loggedOut } = useUser();
+  let accountSection = null;
+  let adminSection = null;
 
-  let profile = null;
   if (isLoading) {
-    profile = <NavLink label="Loading..." />;
-  }
-  if (user) {
-    profile = (
+    accountSection = <NavLink label="Loading..." />;
+  } else if (isLogged) {
+    accountSection = (
       <>
-        <NavLink label="Account">
-          <NavLink component={Link} to="/account/profile" label="Profile" />
-          <NavLink component={Link} to="/account/cart" label="My Cart" />
-          <NavLink
-            //component={Button}
-            color="red"
-            sx={{ color: "red" }}
-            label="Log Out"
-            onClick={async () => {
-              await logOut();
-              mutate(null);
-            }}
-          />
-        </NavLink>
+        <NavLink component={Link} to="/account/profile" label="Profile" />
+        <NavLink component={Link} to="/account/cart" label="My Cart" />
+        <NavLink
+          sx={{ color: "#fa5252" }}
+          label="Log Out"
+          onClick={async () => {
+            await logOut();
+            navigate("/account/login");
+            mutate(null);
+          }}
+        />
       </>
     );
-  }
-  if (loggedOut) {
-    profile = (
+    adminSection = (
+      <NavLink label="Admin" defaultOpened>
+        <NavLink
+          component={Link}
+          to="/products/admin/create"
+          label="Add Product"
+        />
+      </NavLink>
+    );
+  } else {
+    accountSection = (
       <>
         <NavLink component={Link} to="/account/login" label="Log In" />
         <NavLink component={Link} to="/account/signup" label="Sign Up" />
       </>
     );
   }
-
   return (
     <Navbar hiddenBreakpoint="sm" hidden={!opened} width={{ sm: 175, lg: 250 }}>
       <NavLink component={Link} to="/products" label="Products" />
-      {profile}
+      <NavLink label="Account" defaultOpened>
+        {accountSection}
+      </NavLink>
+      {adminSection}
     </Navbar>
   );
 }
-
-//export default memo(NavBar);
